23e3a54201b8b453d92c8c9315d455e1df593fea
SohYiJun <136114973+SohYiJun@users.noreply.github.com>
2023-08-02 03:35:11 +0800

    complete_checkout.php

---
+++ complete_checkout.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+        <title>Competitons</title>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <style>
+        body{
+            background-color: grey;
+        }
+        .complete_checkout{
+            background-color: antiquewhite;
+            margin: auto;
+            border-radius: 13px;
+            border: 1px solid black;
+            height: 300px;
+            width: 500px;
+            margin-top: 15em;
+            text-align:center;
+        }
+        h2{
+            margin-top: 100px;
+            margin-bottom: 2em;
+        }
+    </style>
+    <body>
+
+
+        <div class="complete_checkout">
+
+            <h2>Payment done successfully !!!</h2>
+            <a href="competitionPage.php"><button type="button" class="btn btn-secondary " >Back to Competition Page</button></a>
+            <a href="ViewHistory.php"><button type="button" class="btn btn-primary ">View History</button></a>
+
+        </div>
+
+
+
+
+    </body>
+</html>
